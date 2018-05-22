f7ce7f8a9f48d16fd6ccb402332576d18b4f4906
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-05-22 14:44:02 +0600

    fill struct

--- beans/src/main/java/tech/lapsa/esbd/beans/dao/entities/complex/converter/InsuranceAgentEntityEsbdConverterBean.java
+++ beans/src/main/java/tech/lapsa/esbd/beans/dao/entities/complex/converter/InsuranceAgentEntityEsbdConverterBean.java
@@ -1,10 +1,28 @@
 package tech.lapsa.esbd.beans.dao.entities.complex.converter;
 
+import static tech.lapsa.esbd.beans.dao.entities.complex.Util.*;
+
+import java.util.Optional;
+
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
+import tech.lapsa.esbd.beans.dao.TemporalUtil;
+import tech.lapsa.esbd.dao.entities.complex.SubjectEntityService.SubjectEntityServiceLocal;
+import tech.lapsa.esbd.dao.entities.complex.UserEntityService.UserEntityServiceLocal;
+import tech.lapsa.esbd.dao.entities.dict.BranchEntityService.BranchEntityServiceLocal;
+import tech.lapsa.esbd.dao.entities.dict.InsuranceCompanyEntityService.InsuranceCompanyEntityServiceLocal;
 import tech.lapsa.esbd.domain.complex.InsuranceAgentEntity;
 import tech.lapsa.esbd.domain.complex.InsuranceAgentEntity.InsuranceAgentEntityBuilder;
+import tech.lapsa.esbd.domain.complex.SubjectEntity;
+import tech.lapsa.esbd.domain.complex.UserEntity;
+import tech.lapsa.esbd.domain.dict.BranchEntity;
+import tech.lapsa.esbd.domain.dict.InsuranceCompanyEntity;
+import tech.lapsa.esbd.domain.embedded.ContractInfo;
+import tech.lapsa.esbd.domain.embedded.ContractInfo.ContractInfoBuilder;
+import tech.lapsa.esbd.domain.embedded.RecordOperationInfo;
+import tech.lapsa.esbd.domain.embedded.RecordOperationInfo.RecordOperationInfoBuilder;
 import tech.lapsa.esbd.jaxws.wsimport.MIDDLEMAN;
 import tech.lapsa.java.commons.function.MyOptionals;
 
@@ -12,6 +30,18 @@ import tech.lapsa.java.commons.function.MyOptionals;
 @LocalBean
 public class InsuranceAgentEntityEsbdConverterBean
 	implements AEsbdAttributeConverter<InsuranceAgentEntity, MIDDLEMAN> {
+
+    @EJB
+    private SubjectEntityServiceLocal subjectService;
+
+    @EJB
+    private BranchEntityServiceLocal branchService;
+
+    @EJB
+    private UserEntityServiceLocal userService;
+
+    @EJB
+    private InsuranceCompanyEntityServiceLocal insuranceCompanyService;
 
     @Override
     public MIDDLEMAN convertToEsbdValue(InsuranceAgentEntity source) throws EsbdConversionException {
@@ -31,6 +61,115 @@ public class InsuranceAgentEntityEsbdConverterBean
 		// ID s:int Идентификатор
 		MyOptionals.of(id)
 			.ifPresent(builder::withId);
+	    }
+
+	    {
+		// CONTRACT_DATE s:string дата договора посредника
+		// CONTRACT_NUMBER s:string номер договора посредника
+		final ContractInfoBuilder b1 = ContractInfo.builder();
+
+		MyOptionals.of(source.getCONTRACTDATE())
+			.map(TemporalUtil::dateToLocalDate)
+			.ifPresent(b1::withDateOf);
+
+		MyOptionals.of(source.getCONTRACTNUMBER())
+			.ifPresent(b1::withNumber);
+
+		b1.buildTo(builder::withContract);
+	    }
+
+	    {
+		// LETTER_OF_ATTORNEY_NUMBER s:string номер доверенности
+		// посредника
+		MyOptionals.of(source.getLETTEROFATTORNEYNUMBER())
+			.ifPresent(builder::withLetterOfAttorneyNumber);
+	    }
+
+	    {
+		// BRANCH_ID s:int идентификатор регионального подразделения
+		optField(InsuranceAgentEntity.class,
+			id,
+			branchService::getById,
+			"branch",
+			BranchEntity.class,
+			MyOptionals.of(source.getBRANCHID()))
+				.ifPresent(builder::withBranch);
+	    }
+
+	    {
+		// USER_ID s:int идентификатор пользователя
+		optField(InsuranceAgentEntity.class,
+			id,
+			userService::getById,
+			"user",
+			UserEntity.class,
+			MyOptionals.of(source.getUSERID()))
+				.ifPresent(builder::withUser);
+	    }
+
+	    {
+		// CLIENT_ID s:int идентификатор клиента
+		optField(InsuranceAgentEntity.class,
+			id,
+			subjectService::getById,
+			"subject",
+			SubjectEntity.class,
+			MyOptionals.of(source.getCLIENTID()))
+				.ifPresent(builder::withSubject);
+	    }
+
+	    {
+		final Optional<String> instant = MyOptionals.of(source.getINPUTDATE());
+		final Optional<Integer> author = MyOptionals.of(source.getCREATEDBYUSERID());
+		if (instant.isPresent() || author.isPresent()) {
+
+		    final RecordOperationInfoBuilder b1 = RecordOperationInfo.builder();
+
+		    instant.flatMap(TemporalUtil::optTemporalToInstant)
+			    .ifPresent(b1::withInstant);
+
+		    optField(InsuranceAgentEntity.class,
+			    id,
+			    userService::getById,
+			    "created.author",
+			    UserEntity.class,
+			    author)
+				    .ifPresent(b1::withAuthor);
+
+		    b1.buildTo(builder::withModified);
+		}
+	    }
+
+	    {
+		final Optional<String> instant = MyOptionals.of(source.getRECORDCHANGEDAT());
+		final Optional<Integer> author = MyOptionals.of(source.getCHANGEDBYUSERID());
+		if (instant.isPresent() || author.isPresent()) {
+
+		    final RecordOperationInfoBuilder b1 = RecordOperationInfo.builder();
+
+		    instant.flatMap(TemporalUtil::optTemporalToInstant)
+			    .ifPresent(b1::withInstant);
+
+		    optField(InsuranceAgentEntity.class,
+			    id,
+			    userService::getById,
+			    "modified.author",
+			    UserEntity.class,
+			    author)
+				    .ifPresent(b1::withAuthor);
+
+		    b1.buildTo(builder::withModified);
+		}
+	    }
+
+	    {
+		optField(InsuranceAgentEntity.class,
+			id,
+			insuranceCompanyService::getById,
+			"insurer",
+			InsuranceCompanyEntity.class,
+			MyOptionals.of(source.getSYSTEMDELIMITERID()))
+				.ifPresent(builder::withInsurer);
 	    }
 
 	    return builder.build();
