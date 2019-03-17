be0ec559374c67b10b75b9b628d3d38795b4eac2
Vadim.Isaev <vadim.o.isaev@gmail.com>
2019-03-17 21:07:04 +0300

    policy vehicle ref is optional

--- beans/src/main/java/tech/lapsa/esbd/beans/dao/entities/Util.java
+++ beans/src/main/java/tech/lapsa/esbd/beans/dao/entities/Util.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 import javax.ejb.EJBException;
 
@@ -11,163 +12,182 @@ import tech.lapsa.esbd.dao.NotFound;
 import tech.lapsa.java.commons.function.MyExceptions;
 import tech.lapsa.java.commons.function.MyOptionals;
 import tech.lapsa.java.commons.function.MyStrings;
+import tech.lapsa.java.commons.logging.MyLogger.MyLevel;
 
 final class Util {
 
-	private Util() {
-	}
+    private Util() {
+    }
 
-	static <T> T requireSingle(final List<T> list, final Class<?> clazz, final String keyName, final Object key)
-	        throws EJBException {
-		if (list.size() > 1)
-			throw MyExceptions.illegalArgumentFormat("Too many %1$s (%2$s) with %3$s = '%4$s'", clazz.getSimpleName(), // 1
-			        list.size(), // 2
-			        keyName, // 3
-			        key // 4
-			);
-		return list.get(0);
-	}
+    static <T> T requireSingle(final List<T> list, final Class<?> clazz, final String keyName, final Object key)
+            throws EJBException {
+        if (list.size() > 1)
+            throw MyExceptions.illegalArgumentFormat("Too many %1$s (%2$s) with %3$s = '%4$s'", clazz.getSimpleName(), // 1
+                    list.size(), // 2
+                    keyName, // 3
+                    key // 4
+            );
+        return list.get(0);
+    }
 
-	@FunctionalInterface
-	interface ThrowingFunction<T, R> {
-		R apply(T value) throws Exception;
-	}
+    @FunctionalInterface
+    interface ThrowingFunction<T, R> {
+        R apply(T value) throws Exception;
+    }
 
-	static <T> IllegalArgumentException requireNonEmtyList(final Class<T> targetClazz, final Object targetId,
-	        final String fieldName) {
-		final String message = MyStrings.format("Error fetching %1$s(%2$s).%3$s -> list is empty", targetClazz, // 1,
-		        targetId, // 2
-		        fieldName // 3
-		);
-		return new IllegalArgumentException(message);
-	}
+    static <T> IllegalArgumentException requireNonEmtyList(final Class<T> targetClazz, final Object targetId,
+            final String fieldName) {
+        final String message = MyStrings.format("Error fetching %1$s(%2$s).%3$s -> list is empty", targetClazz, // 1,
+                targetId, // 2
+                fieldName // 3
+        );
+        return new IllegalArgumentException(message);
+    }
 
-	// requireField
+    // requireField
 
-	static <T, F, FI> void requireField(final T target, final Object targetId, final ThrowingFunction<FI, F> fieldGeter,
-	        final Consumer<F> fieldSeter, final String fieldName, final Class<F> fieldClazz, final FI fieldId) {
-		final F fieldObject;
-		try {
-			fieldObject = fieldGeter.apply(fieldId);
-		} catch (final Exception e) {
-			final String message = MyStrings.format("Error fetching %1$s(%2$s).%3$s -> %4$s(%5$s) '%6$s'",
-			        target.getClass().getSimpleName(), // 1,
-			        targetId, // 2
-			        fieldName, // 3
-			        fieldClazz.getSimpleName(), // 4
-			        fieldId, // 5
-			        e.getMessage() // 6
-			);
-			throw new IllegalArgumentException(message, e);
-		}
-		fieldSeter.accept(fieldObject);
-	}
+    static <T, F, FI> void requireField(final T target, final Object targetId, final ThrowingFunction<FI, F> fieldGeter,
+            final Consumer<F> fieldSeter, final String fieldName, final Class<F> fieldClazz, final FI fieldId) {
+        final F fieldObject;
+        try {
+            fieldObject = fieldGeter.apply(fieldId);
+        } catch (final Exception e) {
+            final String message = MyStrings.format("Error fetching %1$s(%2$s).%3$s -> %4$s(%5$s) '%6$s'",
+                    target.getClass().getSimpleName(), // 1,
+                    targetId, // 2
+                    fieldName, // 3
+                    fieldClazz.getSimpleName(), // 4
+                    fieldId, // 5
+                    e.getMessage() // 6
+            );
+            throw new IllegalArgumentException(message, e);
+        }
+        fieldSeter.accept(fieldObject);
+    }
 
-	static <T, F, FI> F reqField(final Class<T> targetClazz, final Object targetId,
-	        final ThrowingFunction<FI, F> entityGeter, final String fieldName, final Class<F> fieldClazz,
-	        final FI fieldId, final Predicate<Throwable> ignoreException) {
+    static <T, F, FI> F reqField(final Class<T> targetClazz, final Object targetId,
+            final ThrowingFunction<FI, F> entityGeter, final String fieldName, final Class<F> fieldClazz,
+            final FI fieldId, final Predicate<Throwable> ignoreException) {
 
-		F fieldObject = null;
-		try {
-			fieldObject = entityGeter.apply(fieldId);
-		} catch (final Exception e) {
-			if (ignoreException == null || !ignoreException.test(e)) {
-				final String message = MyStrings.format("Error fetching %1$s(%2$s).%3$s -> %4$s(%5$s) '%6$s'",
-				        targetClazz.getSimpleName(), // 1,
-				        targetId, // 2
-				        fieldName, // 3
-				        fieldClazz.getSimpleName(), // 4
-				        fieldId, // 5
-				        e.getMessage() // 6
-				);
-				throw new IllegalArgumentException(message, e);
-			}
-		}
-		return fieldObject;
-	}
+        F fieldObject = null;
+        try {
+            fieldObject = entityGeter.apply(fieldId);
+        } catch (final Exception e) {
+            if (ignoreException == null || !ignoreException.test(e)) {
+                final String message = MyStrings.format("Error fetching %1$s(%2$s).%3$s -> %4$s(%5$s) '%6$s'",
+                        targetClazz.getSimpleName(), // 1,
+                        targetId, // 2
+                        fieldName, // 3
+                        fieldClazz.getSimpleName(), // 4
+                        fieldId, // 5
+                        e.getMessage() // 6
+                );
+                throw new IllegalArgumentException(message, e);
+            }
+        }
+        return fieldObject;
+    }
 
-	static <T, F, FI> F reqField(final Class<T> targetClazz, final Object targetId,
-	        final ThrowingFunction<FI, F> fieldGeter, final String fieldName, final Class<F> fieldClazz,
-	        final FI fieldId) {
-		return reqField(targetClazz, targetId, fieldGeter, fieldName, fieldClazz, fieldId, null);
-	}
+    static <T, F, FI> F reqField(final Class<T> targetClazz, final Object targetId,
+            final ThrowingFunction<FI, F> fieldGeter, final String fieldName, final Class<F> fieldClazz,
+            final FI fieldId) {
+        return reqField(targetClazz, targetId, fieldGeter, fieldName, fieldClazz, fieldId, null);
+    }
 
-	// optionalField
+    // optionalField
 
-	static <T, F, FI> Optional<F> optField(final Class<T> targetClazz, final Object targetId,
-	        final ThrowingFunction<FI, F> entityGeter, final String fieldName, final Class<F> fieldClazz,
-	        final Optional<FI> optFieldId, final Predicate<Throwable> ignoreException) {
+    static <T, F, FI> Optional<F> optField(final Class<T> targetClazz, final Object targetId,
+            final ThrowingFunction<FI, F> entityGeter, final String fieldName, final Class<F> fieldClazz,
+            final Optional<FI> optFieldId, final Predicate<Throwable> ignoreException) {
+        return optField(targetClazz, targetId, entityGeter, fieldName, fieldClazz, optFieldId, ignoreException, null);
 
-		if (!optFieldId.isPresent())
-			return Optional.empty();
+    }
 
-		F fieldObject = null;
-		try {
-			fieldObject = entityGeter.apply(optFieldId.get());
-		} catch (final Exception e) {
-			if (ignoreException == null || !ignoreException.test(e)) {
-				final String message = MyStrings.format("Error fetching %1$s(%2$s).%3$s -> %4$s(%5$s) '%6$s'",
-				        targetClazz.getSimpleName(), // 1,
-				        targetId, // 2
-				        fieldName, // 3
-				        fieldClazz.getSimpleName(), // 4
-				        optFieldId.get(), // 5
-				        e.getMessage() // 6
-				);
-				throw new IllegalArgumentException(message, e);
-			}
-		}
-		return MyOptionals.of(fieldObject);
-	}
+    static <T, F, FI> Optional<F> optField(final Class<T> targetClazz, final Object targetId,
+            final ThrowingFunction<FI, F> entityGeter, final String fieldName, final Class<F> fieldClazz,
+            final Optional<FI> optFieldId, final Predicate<Throwable> ignoreException, MyLevel logOnException) {
 
-	static <T, F, FI> Optional<F> optField(final Class<T> targetClazz, final Object targetId,
-	        final ThrowingFunction<FI, F> fieldGeter, final String fieldName, final Class<F> fieldClazz,
-	        final Optional<FI> optFieldId) {
-		return optField(targetClazz, targetId, fieldGeter, fieldName, fieldClazz, optFieldId, null);
-	}
+        if (!optFieldId.isPresent())
+            return Optional.empty();
 
-	static <T, F, FI> Optional<F> optFieldIgnoreFieldNotFound(final Class<T> targetClazz, final Object targetId,
-	        final ThrowingFunction<FI, F> fieldGeter, final String fieldName, final Class<F> fieldClazz,
-	        final Optional<FI> optFieldId) {
-		return optField(targetClazz, targetId, fieldGeter, fieldName, fieldClazz, optFieldId,
-		        e -> (e instanceof NotFound));
-	}
+        F fieldObject = null;
+        try {
+            fieldObject = entityGeter.apply(optFieldId.get());
+        } catch (final Exception e) {
+            final Supplier<String> messageLazySupplier = () -> MyStrings.format(
+                    "Error fetching %1$s(%2$s).%3$s -> %4$s(%5$s) '%6$s'", targetClazz.getSimpleName(), // 1,
+                    targetId, // 2
+                    fieldName, // 3
+                    fieldClazz.getSimpleName(), // 4
+                    optFieldId.get(), // 5
+                    e.getMessage() // 6
+            );
+            if (ignoreException == null || !ignoreException.test(e)) {
+                throw new IllegalArgumentException(messageLazySupplier.get(), e);
+            } else if (logOnException != null) {
+                logOnException.log(messageLazySupplier.get());
+            }
+        }
+        return MyOptionals.of(fieldObject);
+    }
 
-	static <T, F, FI> void optionalField(final T target, final Object targetId,
-	        final ThrowingFunction<FI, F> fieldGeter, final Consumer<F> fieldSeter, final String fieldName,
-	        final Class<F> fieldClazz, final Optional<FI> optFieldId, final Predicate<Throwable> ignoreException) {
-		if (optFieldId.isPresent()) {
-			F fieldObject = null;
-			try {
-				fieldObject = fieldGeter.apply(optFieldId.get());
-			} catch (final Exception e) {
-				if (ignoreException == null || !ignoreException.test(e)) {
-					final String message = MyStrings.format("Error fetching %1$s(%2$s).%3$s -> %4$s(%5$s) '%6$s'",
-					        target.getClass().getSimpleName(), // 1,
-					        targetId, // 2
-					        fieldName, // 3
-					        fieldClazz.getSimpleName(), // 4
-					        optFieldId.get(), // 5
-					        e.getMessage() // 6
-					);
-					throw new IllegalArgumentException(message, e);
-				}
-			}
-			fieldSeter.accept(fieldObject);
-		}
-	}
+    static <T, F, FI> Optional<F> optField(final Class<T> targetClazz, final Object targetId,
+            final ThrowingFunction<FI, F> fieldGeter, final String fieldName, final Class<F> fieldClazz,
+            final Optional<FI> optFieldId) {
+        return optField(targetClazz, targetId, fieldGeter, fieldName, fieldClazz, optFieldId, null, null);
+    }
 
-	static <T, F, FI> void optionalFieldIgnoreFieldNotFound(final T target, final Object targetId,
-	        final ThrowingFunction<FI, F> fieldGeter, final Consumer<F> fieldSeter, final String fieldName,
-	        final Class<F> fieldClazz, final Optional<FI> optFieldId) {
-		optionalField(target, targetId, fieldGeter, fieldSeter, fieldName, fieldClazz, optFieldId,
-		        e -> (e instanceof NotFound));
-	}
+    private static final Predicate<Throwable> IGNORE_NOT_FOUND_EXCEPTION = e -> (e instanceof NotFound);
 
-	static <T, F, FI> void optionalField(final T target, final Object targetId,
-	        final ThrowingFunction<FI, F> fieldGeter, final Consumer<F> fieldSeter, final String fieldName,
-	        final Class<F> fieldClazz, final Optional<FI> optFieldId) {
-		optionalField(target, targetId, fieldGeter, fieldSeter, fieldName, fieldClazz, optFieldId, null);
-	}
+    static <T, F, FI> Optional<F> optFieldIgnoreFieldNotFound(final Class<T> targetClazz, final Object targetId,
+            final ThrowingFunction<FI, F> fieldGeter, final String fieldName, final Class<F> fieldClazz,
+            final Optional<FI> optFieldId) {
+        return optField(targetClazz, targetId, fieldGeter, fieldName, fieldClazz, optFieldId,
+                IGNORE_NOT_FOUND_EXCEPTION, null);
+    }
+
+    static <T, F, FI> Optional<F> optFieldIgnoreFieldNotFound(final Class<T> targetClazz, final Object targetId,
+            final ThrowingFunction<FI, F> fieldGeter, final String fieldName, final Class<F> fieldClazz,
+            final Optional<FI> optFieldId, MyLevel logOnException) {
+        return optField(targetClazz, targetId, fieldGeter, fieldName, fieldClazz, optFieldId,
+                IGNORE_NOT_FOUND_EXCEPTION, logOnException);
+    }
+
+    static <T, F, FI> void optionalField(final T target, final Object targetId,
+            final ThrowingFunction<FI, F> fieldGeter, final Consumer<F> fieldSeter, final String fieldName,
+            final Class<F> fieldClazz, final Optional<FI> optFieldId, final Predicate<Throwable> ignoreException) {
+        if (optFieldId.isPresent()) {
+            F fieldObject = null;
+            try {
+                fieldObject = fieldGeter.apply(optFieldId.get());
+            } catch (final Exception e) {
+                if (ignoreException == null || !ignoreException.test(e)) {
+                    final String message = MyStrings.format("Error fetching %1$s(%2$s).%3$s -> %4$s(%5$s) '%6$s'",
+                            target.getClass().getSimpleName(), // 1,
+                            targetId, // 2
+                            fieldName, // 3
+                            fieldClazz.getSimpleName(), // 4
+                            optFieldId.get(), // 5
+                            e.getMessage() // 6
+                    );
+                    throw new IllegalArgumentException(message, e);
+                }
+            }
+            fieldSeter.accept(fieldObject);
+        }
+    }
+
+    static <T, F, FI> void optionalFieldIgnoreFieldNotFound(final T target, final Object targetId,
+            final ThrowingFunction<FI, F> fieldGeter, final Consumer<F> fieldSeter, final String fieldName,
+            final Class<F> fieldClazz, final Optional<FI> optFieldId) {
+        optionalField(target, targetId, fieldGeter, fieldSeter, fieldName, fieldClazz, optFieldId,
+                e -> (e instanceof NotFound));
+    }
+
+    static <T, F, FI> void optionalField(final T target, final Object targetId,
+            final ThrowingFunction<FI, F> fieldGeter, final Consumer<F> fieldSeter, final String fieldName,
+            final Class<F> fieldClazz, final Optional<FI> optFieldId) {
+        optionalField(target, targetId, fieldGeter, fieldSeter, fieldName, fieldClazz, optFieldId, null);
+    }
 
 }
